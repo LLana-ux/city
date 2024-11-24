8951aa0b32eb41dc959bfba16dc4c674c87295d3
LLana-ux <vasilinagridyushko@gmail.com>
2024-11-24 21:21:44 +0300

    Add files via upload

--- simpleapp/urls.py
+++ simpleapp/urls.py
@@ -18,3 +18,40 @@ urlpatterns = [
 
    path('<int:pk>', ProductDetail.as_view()),
 ]
+
+
+from django.urls import path
+from .views import (
+    ProductsList, ProductDetail, ProductCreate
+)
+
+urlpatterns = [
+    path('', ProductsList.as_view(), name='product_list'),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
+]
+
+from django.urls import path
+from .views import (
+    ProductsList, ProductDetail, ProductCreate, ProductUpdate
+)
+
+urlpatterns = [
+    path('', ProductsList.as_view(), name='product_list'),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+]
+
+from django.urls import path
+from .views import (
+    ProductsList, ProductDetail, ProductCreate, ProductUpdate, ProductDelete
+)
+
+urlpatterns = [
+    path('', ProductsList.as_view(), name='product_list'),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
+]
--- simpleapp/views.py
+++ simpleapp/views.py
@@ -40,8 +40,8 @@ class ProductsList(ListView):
 
     from datetime import datetime
 
-    from django.views.generic import ListView, DetailView
-    from .models import Product
+from django.views.generic import ListView, DetailView
+from .models import Product
 
     class ProductsList(ListView):
         model = Product
@@ -70,3 +70,184 @@ class ProductsList(ListView):
             context['time_now'] = datetime.utcnow()
             context['next_sale'] = "Распродажа в среду!"
             return context
+
+from django.views.generic import ListView, DetailView
+from .models import Product
+    class ProductsList(ListView):
+        model = Product
+        ordering = 'name'
+        template_name = 'products.html'
+        context_object_name = 'products'
+        paginate_by = 2
+
+from django.views.generic import ListView, DetailView
+from .models import Product
+from .filters import ProductFilter
+
+    class ProductsList(ListView):
+        model = Product
+        ordering = 'name'
+        template_name = 'products.html'
+        context_object_name = 'products'
+        paginate_by = 2
+
+        # Переопределяем функцию получения списка товаров
+        def get_queryset(self):
+            # Получаем обычный запрос
+            queryset = super().get_queryset()
+            # Используем наш класс фильтрации.
+            # self.request.GET содержит объект QueryDict, который мы рассматривали
+            # в этом юните ранее.
+            # Сохраняем нашу фильтрацию в объекте класса,
+            # чтобы потом добавить в контекст и использовать в шаблоне.
+            self.filterset = ProductFilter(self.request.GET, queryset)
+            # Возвращаем из функции отфильтрованный список товаров
+            return self.filterset.qs
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            # Добавляем в контекст объект фильтрации.
+            context['filterset'] = self.filterset
+            return context
+
+    class ProductDetail(DetailView):
+        model = Product
+        template_name = 'product.html'
+        context_object_name = 'product'
+
+
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView
+)
+
+from .filters import ProductFilter
+from .forms import ProductForm
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+
+
+class ProductCreate(CreateView):
+
+    form_class = ProductForm
+
+    model = Product
+
+    template_name = 'product_edit.html'
+
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView
+)
+
+from .filters import ProductFilter
+from .forms import ProductForm
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+
+class ProductCreate(CreateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+    from django.urls import reverse_lazy
+    from django.views.generic import (
+        ListView, DetailView, CreateView, UpdateView, DeleteView
+    )
+
+    from .filters import ProductFilter
+    from .forms import ProductForm
+    from .models import Product
+
+    class ProductsList(ListView):
+        model = Product
+        ordering = 'name'
+        template_name = 'products.html'
+        context_object_name = 'products'
+        paginate_by = 2
+
+        def get_queryset(self):
+            queryset = super().get_queryset()
+            self.filterset = ProductFilter(self.request.GET, queryset)
+            return self.filterset.qs
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['filterset'] = self.filterset
+            return context
+
+    class ProductDetail(DetailView):
+        model = Product
+        template_name = 'product.html'
+        context_object_name = 'product'
+
+    class ProductCreate(CreateView):
+        form_class = ProductForm
+        model = Product
+        template_name = 'product_edit.html'
+
+    class ProductUpdate(UpdateView):
+        form_class = ProductForm
+        model = Product
+        template_name = 'product_edit.html'
+
+    # Представление удаляющее товар.
+    class ProductDelete(DeleteView):
+        model = Product
+        template_name = 'product_delete.html'
+        success_url = reverse_lazy('product_list')
